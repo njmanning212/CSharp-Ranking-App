f93ad95a297ed7288479376b468b50a07ac81dc3
Nicholas Manning <njmanning212@gmail.com>
2023-10-10 12:54:29 -0500

    add ItemController

--- Models/ItemModel.cs
+++ Models/ItemModel.cs
@@ -7,7 +7,7 @@ public class ItemModel
 {
   public int Id { get; set; }
   public string Title { get; set; }
-  public int ImageID {get; set;}
+  public int ImageId {get; set;}
   public int Ranking { get; set; }
   public int ItemType { get; set; }
   
