b42efdd677490a5e8f490d159d5d171658d22b5f
Nicholas Manning <njmanning212@gmail.com>
2023-10-11 10:35:41 -0500

    refactor useEffect in RankItems Component, change dataType from consant and set initial state

--- ClientApp/src/components/RankItems.js
+++ ClientApp/src/components/RankItems.js
@@ -3,23 +3,22 @@ import { useEffect, useState } from "react";
 const RankItems = () => {
 
   const [items, setItems] = useState([])
-  const dataType = 1
+  const [dataType, setDataType] = useState(1)
 
   useEffect(() => {
-    fetch(`item/${dataType}`)
-      .then( (results) => {
-        return results.json()
-      })
-      .then(data => {
-        setItems(data)
-      })
-  },[])
+    const fetchData  = async () => {
+      const response = await fetch(`item/${dataType}`)
+      const data = await response.json()
+      setItems(data)
+    }
+    fetchData()
+  },[dataType])
 
   return ( 
     <main>
       {
-        (items != null) ? items.map((item) => 
-          <h3>
+        (items != null) ? items.map((item, index) => 
+          <h3 key={index}>
             {item.title}
           </h3>
         ) :
